28b743eb7bd4bf42100db5bb1a51bc87b4a9a2d4
Sounak Kar <skarunivarse@gmail.com>
2023-10-26 14:16:05 +0530

    Update main.cpp
    FIXED multi token / token size limit
    
    not can randgen length[int] splitts[int] so out = AAA...-AAA...-etc

--- RANDGEN/main.cpp
+++ RANDGEN/main.cpp
@@ -6,46 +6,51 @@ using namespace std;
 
 string RandomString(int len)
 {
-   string str = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-   string newstr;
-   int pos;
-   while(newstr.size() != len) {
-    pos = ((rand() % (str.size() - 1)));
-    newstr += str.substr(pos,1);
-   }
-   return newstr;
+    string str = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string newstr;
+    int pos;
+    while (newstr.size() != len) {
+        pos = ((rand() % (str.size() - 1)));
+        newstr += str.substr(pos, 1);
+    }
+    return newstr;
 }
 
-int main(int argc,char *argv[])
+int main(int argc, char* argv[])
 {
-	int lnth=128;
-string random_str;
-int split=0;
-  if(argc > 3){
-        cout<<"Too many Agruments!!"<<endl <<"Default out 128 bit only Numaric value allowed with no upper limit Like: randgen 1024 0."<<endl;
-  }else if(argc < 1){
-  	
-  	cout<<"Too few Agruments!!"<<endl <<"Default out 128 bit only Numaric value allowed with no upper limit Like: randgen 1024 0."<<endl;
-  }
-  else{
+    int lnth = 128;
+    string random_str;
+    int split = 1;
+    if (argc > 3) {
+        cout << "Too many Agruments!!" << endl << "Default out 128 bit only Numaric value allowed with no upper limit Like: randgen 1024 0." << endl;
+    }else {
+        if (argv[1] != NULL) {
+            if (atoi(argv[1]) >= 1) {
+                lnth = atoi(argv[1]);
+            }
+            if (argv[2] != NULL) {
+                if (atoi(argv[2]) >= 1) {
+                    split = atoi(argv[2]);
+                }
+            }
+            else {
+                split = 1;
+            }
+        }
+        else {
+            lnth = 128;
+        }
+        
 
- if(atoi(argv[1])>=1){
-         lnth=atoi(argv[1]);
-}
- 	
- if(atoi(argv[2])>= 1) {
-    split=atoi(argv[2]);	
-}
+        srand(time(0));
 
-   srand(time(0));
-   
-for (int i = 0; i <= split; i++) {
-  if(i >=1)
-{
-random_str += "-";	
-}
-random_str += RandomString(lnth);
-}
-   cout  << random_str << endl ;
-}
+        for (int i = 1; i <= split; i++) {
+            if (i > 1)
+            {
+                random_str += "-";
+            }
+            random_str += RandomString(lnth);
+        }
+        cout << random_str << endl;
+    }
 }
