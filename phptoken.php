7f86fc0bf7cf4d3cdea27eded67b9b47d16c2ed0
Sounak Kar <skarunivarse@gmail.com>
2023-10-26 12:38:05 +0530

    Update phptoken.php

--- phptoken.php
+++ phptoken.php
@@ -3,7 +3,7 @@ function getToken($length)
     $str_result = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     return substr(str_shuffle($str_result),0, $length);
 }
-function getToken3($length)
+function getToken2($length)
 {
     $str_result = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     return substr(str_shuffle($str_result),0, $length).'-'.substr(str_shuffle($str_result),0, $length).'-'.substr(str_shuffle($str_result),0, $length);
